e7ce347dfd7121b3513e371e3309f1e4f62eb560
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-10-25 13:29:48 -0300

    feat(aula-1/index.js/aula-3/ex/index.js/index.js/aula-4/comentarios.js): Ao ver o curso de javascript do basico ao avancado, no final fiz suas logicas aprendindas para fixar o conteudo! -@angeloneves19

---
+++ aula-4/comentarios.js
@@ -0,0 +1,13 @@
+//Comentario para 1 linha!
+
+/*
+Comentarios maiores
+ */
+
+console.log("Não vejo comentarios além desse")
+
+/**
+ * Topicos e
+ * Comentarios 
+ * Profissionais e bonitinhos
+ */
